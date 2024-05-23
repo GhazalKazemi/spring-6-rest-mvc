329c7b47d94160016aea7f9188af5da7593776b6
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-05-23 12:15:25 +1000

    Set up getBeerById request

--- src/main/java/com/ghazal/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/ghazal/spring6restmvc/controller/BeerController.java
@@ -6,7 +6,9 @@ import com.ghazal.spring6restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,16 +17,18 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer/")
 public class BeerController {
 
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
         log.debug("Inside listBeers controller");
         return beerService.listBeers();
     }
-    public Beer getBeerById(UUID id){
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("id") UUID id){
         log.debug("Inside getBeerById controller, Beer Id: {}", id.toString());
         return beerService.getBeerById(id);
     }
