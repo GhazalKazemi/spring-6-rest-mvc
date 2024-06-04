2aca5b4e74740cdd8b591696a4d24ed604e39627
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-06-04 10:12:48 +1000

    Implement data transaction logic for findAll and findBy methods

---
+++ src/main/java/com/ghazal/spring6restmvc/service/BeerServiceJpa.java
@@ -0,0 +1,56 @@
+package com.ghazal.spring6restmvc.service;
+
+import com.ghazal.spring6restmvc.mapper.BeerMapper;
+import com.ghazal.spring6restmvc.model.BeerDTO;
+import com.ghazal.spring6restmvc.repository.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class BeerServiceJpa implements BeerService {
+
+    private final BeerRepository beerRepository;
+
+    private final BeerMapper beerMapper;
+    @Override
+    public List<BeerDTO> listBeers() {
+        return beerRepository.findAll()
+                .stream()
+                .map(beerMapper::beerToBeerDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<BeerDTO> getBeerById(UUID id) {
+        return Optional.ofNullable(beerMapper.beerToBeerDto(beerRepository.findById(id)
+                .orElse(null)));
+    }
+
+    @Override
+    public BeerDTO addBeer(BeerDTO beer) {
+        return null;
+    }
+
+    @Override
+    public void updateBeerById(UUID id, BeerDTO beer) {
+
+    }
+
+    @Override
+    public void deleteBeerById(UUID id) {
+
+    }
+
+    @Override
+    public void partialUpdateBeerById(UUID id, BeerDTO beer) {
+
+    }
+}
---
+++ src/main/java/com/ghazal/spring6restmvc/service/CustomerServiceJpa.java
@@ -0,0 +1,56 @@
+package com.ghazal.spring6restmvc.service;
+
+import com.ghazal.spring6restmvc.mapper.CustomerMapper;
+import com.ghazal.spring6restmvc.model.CustomerDTO;
+import com.ghazal.spring6restmvc.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CustomerServiceJpa implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    @Override
+    public List<CustomerDTO> getCustomers() {
+        return customerRepository.findAll()
+                .stream()
+                .map(customerMapper::customerToCustomerDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<CustomerDTO> getCustomerById(UUID id) {
+        return Optional.ofNullable(customerMapper.customerToCustomerDto(customerRepository.findById(id)
+                .orElse(null)));
+    }
+
+    @Override
+    public CustomerDTO addCustomer(CustomerDTO customer) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomerById(UUID id, CustomerDTO customer) {
+
+    }
+
+    @Override
+    public void deleteCustomerById(UUID id) {
+
+    }
+
+    @Override
+    public void partialUpdateCustomerById(UUID id, CustomerDTO customer) {
+
+    }
+}
