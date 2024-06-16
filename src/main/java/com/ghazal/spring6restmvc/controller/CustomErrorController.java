99fe8d56169880c9155f31fe2985979cd2e12443
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-06-17 09:57:47 +1000

    Create custom constraint violation handler

--- src/main/java/com/ghazal/spring6restmvc/controller/CustomErrorController.java
+++ src/main/java/com/ghazal/spring6restmvc/controller/CustomErrorController.java
@@ -1,5 +1,6 @@
 package com.ghazal.spring6restmvc.controller;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,12 +10,26 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class CustomErrorController {
     @ExceptionHandler
     ResponseEntity handleJPATransactionViolation(TransactionSystemException exception){
-        return ResponseEntity.badRequest().build();
+        ResponseEntity.BodyBuilder badRequestResponseEntity = ResponseEntity.badRequest();
+        if (exception.getCause().getCause() instanceof ConstraintViolationException){
+            ConstraintViolationException constraintViolationException = (ConstraintViolationException) exception.getCause().getCause();
+            List errorList = constraintViolationException.getConstraintViolations()
+                    .stream()
+                    .map(constraintViolation -> {
+                        Map<String, String> errorMap = new HashMap<>();
+                        errorMap.put(constraintViolation.getPropertyPath().toString(),
+                                constraintViolation.getMessage());
+                        return errorMap;
+                            }).toList();
+            return badRequestResponseEntity.body(errorList);
+        }
+        return badRequestResponseEntity.build();
     }
     @ExceptionHandler(org.springframework.web.bind.MethodArgumentNotValidException.class)
     ResponseEntity handleBindErrors(MethodArgumentNotValidException exception){
