cebb105e5d0d0c3d7ff30ecfa42c5db2391a2f90
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-06-30 09:35:43 +1000

    Persist beer data from csv file  to mysql repository

--- src/main/java/com/ghazal/spring6restmvc/bootstrap/BootstrapData.java
+++ src/main/java/com/ghazal/spring6restmvc/bootstrap/BootstrapData.java
@@ -2,16 +2,24 @@ package com.ghazal.spring6restmvc.bootstrap;
 
 import com.ghazal.spring6restmvc.entity.Beer;
 import com.ghazal.spring6restmvc.entity.Customer;
+import com.ghazal.spring6restmvc.model.BeerCSVRecord;
 import com.ghazal.spring6restmvc.model.BeerStyle;
 import com.ghazal.spring6restmvc.repository.BeerRepository;
 import com.ghazal.spring6restmvc.repository.CustomerRepository;
+import com.ghazal.spring6restmvc.service.BeerCSVService;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
 
 @Component
 @AllArgsConstructor
@@ -21,12 +29,48 @@ public class BootstrapData implements CommandLineRunner {
 
     private final CustomerRepository customerRepository;
 
+    private final BeerCSVService beerCSVService;
 
+
+    @Transactional
     @Override
     public void run(String... args) throws Exception {
         loadBeers();
         loadCustomers();
+        loadCSVData();
     }
+
+    private void loadCSVData() throws FileNotFoundException {
+        if (beerRepository.count() < 10){
+            File file = ResourceUtils.getFile("classpath:csvdata/beers.csv");
+
+            List<BeerCSVRecord> beerRecords = beerCSVService.convertCSV(file);
+
+            beerRecords.forEach(beerCSVRecord -> {
+                BeerStyle beerStyle = switch (beerCSVRecord.getStyle()) {
+                    case "American Pale Lager" -> BeerStyle.LAGER;
+                    case "American Pale Ale (APA)", "American Black Ale", "Belgian Dark Ale", "American Blonde Ale" ->
+                            BeerStyle.ALE;
+                    case "American IPA", "American Double / Imperial IPA", "Belgian IPA" -> BeerStyle.IPA;
+                    case "American Porter" -> BeerStyle.PORTER;
+                    case "Oatmeal Stout", "American Stout" -> BeerStyle.STOUT;
+                    case "Saison / Farmhouse Ale" -> BeerStyle.SAISON;
+                    case "Fruit / Vegetable Beer", "Winter Warmer", "Berliner Weissbier" -> BeerStyle.WHEAT;
+                    case "English Pale Ale" -> BeerStyle.PALE_ALE;
+                    default -> BeerStyle.PILSNER;
+                };
+
+                beerRepository.save(Beer.builder()
+                        .beerName(StringUtils.abbreviate(beerCSVRecord.getBeer(), 50))
+                        .beerStyle(beerStyle)
+                        .price(BigDecimal.TEN)
+                        .upc(beerCSVRecord.getRow().toString())
+                        .quantityOnHand(beerCSVRecord.getCount())
+                        .build());
+            });
+        }
+    }
+
     private void loadBeers() {
         if(beerRepository.count() == 0){
             Beer balterPaleAle = Beer.builder()
--- src/test/java/com/ghazal/spring6restmvc/bootstrap/BootstrapDataTest.java
+++ src/test/java/com/ghazal/spring6restmvc/bootstrap/BootstrapDataTest.java
@@ -2,31 +2,38 @@ package com.ghazal.spring6restmvc.bootstrap;
 
 import com.ghazal.spring6restmvc.repository.BeerRepository;
 import com.ghazal.spring6restmvc.repository.CustomerRepository;
+import com.ghazal.spring6restmvc.service.BeerCSVService;
+import com.ghazal.spring6restmvc.service.BeerCSVServiceImpl;
+import org.hibernate.annotations.Immutable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
+@Import(BeerCSVServiceImpl.class)
 class BootstrapDataTest {
     @Autowired
     BeerRepository beerRepository;
     @Autowired
     CustomerRepository customerRepository;
+    @Autowired
+    BeerCSVService beerCSVService;
 
     BootstrapData bootstrapData;
 
     @BeforeEach
     void setUp() {
-        bootstrapData = new BootstrapData(beerRepository, customerRepository);
+        bootstrapData = new BootstrapData(beerRepository, customerRepository, beerCSVService);
     }
 
     @Test
     void run() throws Exception {
         bootstrapData.run(null);
-        assertThat(beerRepository.count()).isEqualTo(3);
+        assertThat(beerRepository.count()).isEqualTo(2413);
         assertThat(customerRepository.count()).isEqualTo(3);
     }
 }
